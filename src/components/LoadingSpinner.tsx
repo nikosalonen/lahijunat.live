7b9c86ae2196dcdb2d72c766bf9fcf93258fed2b
Niko Salonen <nikotsalonen@gmail.com>
2025-02-13 18:20:53 +0200

    Add loading spinner component with Tailwind CSS styling

---
+++ src/components/LoadingSpinner.tsx
@@ -0,0 +1,7 @@
+export default function LoadingSpinner() {
+  return (
+    <div class="flex justify-center items-center h-screen">
+      <div class="animate-spin rounded-full h-16 w-16 border-t-2 border-b-2 border-blue-500"></div>
+    </div>
+  );
+}
