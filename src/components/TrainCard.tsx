276095030166750ce46ef2b2b861b299df7ed6a6
Niko Salonen <nikotsalonen@gmail.com>
2025-02-14 11:52:27 +0200

    feat: add visual warning for cancelled trains in TrainCard

--- src/components/TrainCard.tsx
+++ src/components/TrainCard.tsx
@@ -54,8 +54,30 @@ export default function TrainCard({
 				<div class="flex items-center gap-4 flex-1">
 					{/* Train identifier */}
 					{train.commuterLineID && (
-						<div class="h-12 w-12 bg-green-100 text-green-800 rounded-full flex items-center justify-center text-lg font-medium">
+						<div
+							class={`h-12 w-12 ${train.cancelled ? "bg-red-100 text-red-800" : "bg-green-100 text-green-800"} rounded-full flex items-center justify-center text-lg font-medium`}
+						>
 							{train.commuterLineID}
+						</div>
+					)}
+
+					{/* Warning triangle for cancelled trains */}
+					{train.cancelled && (
+						<div class="text-red-600" title="Juna peruttu">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								viewBox="0 0 24 24"
+								fill="currentColor"
+								class="w-6 h-6"
+								aria-label="Juna peruttu"
+							>
+								<title>Juna peruttu</title>
+								<path
+									fill-rule="evenodd"
+									d="M9.401 3.003c1.155-2 4.043-2 5.197 0l7.355 12.748c1.154 2-.29 4.5-2.599 4.5H4.645c-2.309 0-3.752-2.5-2.598-4.5L9.4 3.003zM12 8.25a.75.75 0 01.75.75v3.75a.75.75 0 01-1.5 0V9a.75.75 0 01.75-.75zm0 8.25a.75.75 0 100-1.5.75.75 0 000 1.5z"
+									clip-rule="evenodd"
+								/>
+							</svg>
 						</div>
 					)}
 
