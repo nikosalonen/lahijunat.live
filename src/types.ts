e96cfad8c96ce6cd6c45e1b806b08c0c9056eeed
Niko Salonen <nikotsalonen@gmail.com>
2025-02-15 15:27:26 +0200

    style: Standardize indentation to tabs across files

--- src/types.ts
+++ src/types.ts
@@ -1,28 +1,28 @@
 export interface Station {
-  name: string;
-  shortCode: string;
+	name: string;
+	shortCode: string;
 }
 
 export interface Train {
-  operatorUICCode: string;
-  trainCategory: string;
-  trainType: string;
-  cancelled: boolean;
-  commuterLineID: string;
-  trainNumber: string;
-  timeTableRows: TimeTableRow[];
+	operatorUICCode: string;
+	trainCategory: string;
+	trainType: string;
+	cancelled: boolean;
+	commuterLineID: string;
+	trainNumber: string;
+	timeTableRows: TimeTableRow[];
 }
 
 export interface TimeTableRow {
-  trainStopping: boolean;
-  stationShortCode: string;
-  type: string;
-  commercialStop: boolean;
-  commercialTrack: string;
-  cancelled: boolean;
-  scheduledTime: string;
-  liveEstimateTime?: string;
-  actualTime?: string;
-  differenceInMinutes?: number;
-  station: Station;
+	trainStopping: boolean;
+	stationShortCode: string;
+	type: string;
+	commercialStop: boolean;
+	commercialTrack: string;
+	cancelled: boolean;
+	scheduledTime: string;
+	liveEstimateTime?: string;
+	actualTime?: string;
+	differenceInMinutes?: number;
+	station: Station;
 }
