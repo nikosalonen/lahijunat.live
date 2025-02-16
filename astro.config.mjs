f58caf4429e91434c4e1501a432b02e17863c11e
Niko Salonen <nikotsalonen@gmail.com>
2025-02-16 12:19:45 +0200

    chore: Add site URL to Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,15 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
-import preact from '@astrojs/preact';
+import preact from "@astrojs/preact";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  },
+	site: "https://lahijunat.live",
+	vite: {
+		plugins: [tailwindcss()],
+	},
 
-  integrations: [preact()]
+	integrations: [preact()],
 });
