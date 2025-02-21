d3a4cfeff0ad7e23535d7732e31c7129f365813b
Niko Salonen <nikotsalonen@gmail.com>
2025-02-21 14:38:22 +0200

    refactor: Improve StationOption accessibility with detailed linting comment

--- src/components/StationOption.tsx
+++ src/components/StationOption.tsx
@@ -21,7 +21,7 @@ function StationOption({
 			onClick={() => onSelect(station)}
 			onKeyDown={handleKeyDown}
 			tabIndex={0}
-			// biome-ignore lint/a11y/useSemanticElements: <explanation>
+			// biome-ignore lint/a11y/useSemanticElements: This is not inside a select element and option must be inside a select element
 			role="option"
 			aria-selected={isSelected}
 			class="p-2 hover:bg-gray-100 dark:hover:bg-gray-700 cursor-pointer dark:text-white"
