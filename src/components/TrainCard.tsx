f037a10966c2d1466092415e118486f2fd2adaa1
Niko Salonen <nikotsalonen@gmail.com>
2025-04-16 08:52:05 +0300

    refactor: remove cancelled train warning from TrainCard component

--- src/components/TrainCard.tsx
+++ src/components/TrainCard.tsx
@@ -132,26 +132,6 @@ export default function TrainCard({
 						</div>
 					)}
 
-					{/* Warning triangle for cancelled trains */}
-					{train.cancelled && (
-						<div class="flex-shrink-0 text-red-600" title={t('cancelled')}>
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								viewBox="0 0 24 24"
-								fill="currentColor"
-								class="w-7 h-7"
-								aria-label={t('cancelled')}
-							>
-								<title>{t('cancelled')}</title>
-								<path
-									fill-rule="evenodd"
-									d="M9.401 3.003c1.155-2 4.043-2 5.197 0l7.355 12.748c1.154 2-.29 4.5-2.599 4.5H4.645c-2.309 0-3.752-2.5-2.598-4.5L9.4 3.003zM12 8.25a.75.75 0 01.75.75v3.75a.75.75 0 01-1.5 0V9a.75.75 0 01.75-.75zm0 8.25a.75.75 0 100-1.5.75.75 0 000 1.5z"
-									clip-rule="evenodd"
-								/>
-							</svg>
-						</div>
-					)}
-
 					{/* Main train info */}
 					<div class="space-y-2 min-w-0 flex-1">
 						<div class="flex flex-col gap-1">
